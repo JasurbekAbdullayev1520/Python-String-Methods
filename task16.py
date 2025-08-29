85c0ea98aa23d5f35ea2e2a04fce7479fabdda29
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-29 21:47:26 +0500

    bajardim

---
+++ task16.py
@@ -0,0 +1,9 @@
+matn=input('Matn kiriting: ')
+
+matn1=input("qaysi so'zni almashtirmaoqchisiz: ")
+
+matn2=input("o'rniga nima kiritmoqchisiz: ")
+
+yangi_matn=matn.replace(matn1,matn2)
+
+print(yangi_matn)
